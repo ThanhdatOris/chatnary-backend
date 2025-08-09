82966fb23bd131935491035b0981cc23b48199ec
DatOris <ntdathttt2211003@student.ctuet.edu.vn>
2025-08-09 23:10:39 +0700

    test connect

--- server.js
+++ server.js
@@ -12,3 +12,7 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.get('/api/hello', (req, res) => {
+  res.json({ message: 'Hello from backend!' });
+});
